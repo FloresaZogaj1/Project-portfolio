9af39ded22084cdb28dc045dc5b910095d8dad31
Floresa <floresaa.zogaj@gmail.com>
2025-04-22 22:07:27 +0200

    Deploy project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css'; 
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import ServicesSection from './components/ServicesSection';
+import Services from './components/Services'; 
+import About from './components/About';
+import ProjectsSection from './components/ProjectSection';
+import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Hero />
+            <ServicesSection />
+            <About />
+            <ProjectsSection />
+            <ContactForm />
+          </>
+        } />
+
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<ProjectsSection/>} />
+        <Route path="/contact" element={<ContactForm />} />
+
+        {/* Mund të shtosh edhe rrugë të tjera këtu */}
+      </Routes>
     </div>
   );
 }
